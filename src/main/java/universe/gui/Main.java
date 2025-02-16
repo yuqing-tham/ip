e9022431c3b7aebaef649a321b95cd78da44637b
yuqing-tham <yuqing.tham@gmail.com>
2025-02-16 13:37:23 +0800

    Change App Icon and Title

--- src/main/java/universe/gui/Main.java
+++ src/main/java/universe/gui/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import universe.Universe;
@@ -26,6 +27,11 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setMinHeight(220);
+            stage.setMinWidth(417);
+            stage.setMaxWidth(417);
+            stage.setTitle("Universe");
+            stage.getIcons().add(new Image("/images/Universe.png"));
             fxmlLoader.<MainWindow>getController().setUniverse(universe);
             stage.show();
         } catch (IOException e) {
